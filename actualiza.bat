c84ba0e195bfc118d2daa1a07ccf7837250c3b74
Manel <manelmonteroprofe@gmail.com>
2026-05-26 14:00:05 +0200

    Actualizado 26 Mayo 2026

---
+++ actualiza.bat
@@ -0,0 +1,8 @@
+echo off
+cls
+rem ACTUALIZA GIT HUB
+echo *** ACTUALIZANDO GIT HUB CURSO ***
+git add .
+git commit -m "Actualizado 26 Mayo 2026"
+git push origin master
+echo *** ACTUALIZACION FINALIZADA ***
